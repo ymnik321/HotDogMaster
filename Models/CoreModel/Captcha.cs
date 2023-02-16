27b6fd6e7d84c96824a0c77ebe4cce173f3846bd
SCHavelevVE <SCHavelevVE@402D-1>
2023-02-16 16:09:14 +0700

    Добавлено CoreModel, Captcha создано DataModels

---
+++ Models/CoreModel/Captcha.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataModels.Helpers;
+
+namespace CoreModel
+{
+    public static class Captcha
+    {
+        public static (string HashCode, byte[] Image) GenerateCaptchaInfo()
+        {
+            var code = (new Random()).Next(100000, 1000000).ToString();
+            var hash = HashHundle.GetHashString(code);
+            var image = (new CaptchaGen.SkiaSharp.CaptchaGenerator()).GenerateImageAsByteArray(code);
+            return (hash, image);
+        }
+    }
+}
---
+++ Models/DataModels/Helpers/HashHundle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace DataModels.Helpers
+{
+    public static class HashHundle
+    {
+        public static string GetHashString(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+                throw new ArgumentException("Empty code");
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(code));
+            return Convert.ToBase64String(hash);
+        }
+        public static bool VerifyHashedString(string hashedCode, string code)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+                return false;
+            return hashedCode == GetHashString(code);
+        }
+    }
+}
